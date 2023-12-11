398b472f48846ef0a920b3284fbc5243df67513f
= <=>
2023-12-11 21:42:23 +0700

    update screen

---
+++ DoubleFours/CollectionImage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoubleFours
+{
+    public class CollectionImage
+    {
+        //tao ma tran chia nho hinh png pokemongame.png thanh cac image bitmap rieng biet
+        //31 con voi 3 trang thai
+        public static Bitmap[,] PokemonImage; //{ get; set; }
+        //ma trận hình là 1 biến dùng chung sẽ khởi tạo ngay đầu form1
+        public CollectionImage(Bitmap bigbitmap, int PoNumber, int PoAction)
+        {
+            PokemonImage = CreatePokemon(bigbitmap, PoNumber, PoAction);
+        }
+        //chia nho image
+        public static Bitmap[,] CreatePokemon(Bitmap bigbitmap, int PoNumber, int PoAction)
+        {
+            Bitmap[,] bm = new Bitmap[PoNumber, PoAction];
+            float _Height = (float)bigbitmap.Height / PoAction;//chieu` doc//cac trang thai pokemon
+            float _Width = (float)bigbitmap.Width / PoNumber;//chieu ngang//31 con pokemon
+            for (int i = 0; i < PoNumber; i++)
+                for (int j = 0; j < PoAction; j++)
+                {
+                    bm[i, j] = bigbitmap.Clone(new Rectangle((int)(i * _Width), (int)(j * _Height)
+                        , (int)_Width, (int)_Height), System.Drawing.Imaging.PixelFormat.DontCare);
+                }
+            return bm;
+        }
+    }
+}
---
+++ DoubleFours/InfinityStones.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoubleFours
+{
+    public class InfinityStones
+    {
+        private PictureBox ptbstone2;
+        private PictureBox ptbstone3;
+        private PictureBox ptbstone4;
+        private PictureBox ptbstone5;
+        private PictureBox ptbstone6;
+        private PictureBox ptbstone1;
+        private int numstone;
+        private int[,] stone = new int[6, 2];
+        ChessBoardManager chessBoard;
+        public InfinityStones(PictureBox ptbstone1, PictureBox ptbstone2, PictureBox ptbstone3, PictureBox ptbstone4, PictureBox ptbstone5, PictureBox ptbstone6,ChessBoardManager chessBoard)
+        {
+            this.ptbstone1 = ptbstone1;
+            this.ptbstone2 = ptbstone2;
+            this.ptbstone3 = ptbstone3;
+            this.ptbstone4 = ptbstone4;
+            this.ptbstone5 = ptbstone5;
+            this.ptbstone6 = ptbstone6;
+            this.chessBoard = chessBoard;
+            resetStone();
+        }
+        public void resetStone()
+        {
+            numstone = 0;
+            stoneLocation();
+
+            ptbstone1.Visible = false;
+
+            ptbstone2.Visible = false;
+
+            ptbstone3.Visible = false;
+
+            ptbstone4.Visible = false;
+
+            ptbstone5.Visible = false;
+
+            ptbstone6.Visible = false;
+        }
+        private void stoneLocation()
+        {
+            Random rd = new Random();
+            if (chessBoard.coupleRemain*2 > 30)//búng tay 1 lần xong mà trên 30 pokemon vẫn tiếp tục tìm đá
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    int x;
+                    int y;
+                    y = rd.Next(1, Cons.CHESS_BOARD_HEIGHT + 1);//update
+                    x = rd.Next(1, Cons.CHESS_BOARD_WIDTH + 1);
+                    if (chessBoard.matrix[x, y].BackgroundImage != null)
+                    {
+                        stone[i, 0] = x;
+                        stone[i, 1] = y;
+                    }
+                }
+            }
+            else
+            {
+                stone = null;
+            }
+        }
+        public void CheckStone(Chess poke)
+        {
+            int kindofstone;
+            if (stone != null)
+                for (int i = 0; i < 6; i++)
+                {
+                    if (poke.X == stone[i, 0] && poke.Y == stone[i, 1])
+                    {
+                        numstone++;
+                        kindofstone = i;
+                        UpdateStone(kindofstone);
+                        DFSoundPlayer.media_foundstone.Play();
+                        break;
+                    }
+                }
+
+        }
+        private void UpdateStone(int kindofstone)
+        {
+            switch (kindofstone)
+            {
+                case 0:
+                    ptbstone1.Visible = true;
+                    break;
+                case 1:
+                    ptbstone2.Visible = true;
+                    break;
+                case 2:
+                    ptbstone3.Visible = true;
+                    break;
+                case 3:
+                    ptbstone4.Visible = true;
+                    break;
+                case 4:
+                    ptbstone5.Visible = true;
+                    break;
+                case 5:
+                    ptbstone6.Visible = true;
+                    break;
+            }
+        }
+        public bool canSnap()
+        {
+            if (numstone == 6) return true;
+            else return false;
+        }
+    }
+}
